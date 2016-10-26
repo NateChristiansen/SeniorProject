e08ee013ab2a0b2ee7153d379d86e24e7aae12b6
jakewheeler <jakezro00@aim.com>
2016-10-26 17:40:00 -0400

    fixed merge conflicts but path is gone

--- Prototype/Assets/Scripts/MoveCar.cs
+++ Prototype/Assets/Scripts/MoveCar.cs
@@ -3,11 +3,7 @@ using System.Collections;
 
 public class MoveCar : MonoBehaviour {
 
-<<<<<<< HEAD
     public BezierCurve path;
-=======
-    public BezierPath path;
->>>>>>> 84ad8a2f0574c8f4902090857981526688a4350f
     public float speed;
     private float t = 0;
 
@@ -20,12 +16,8 @@ public class MoveCar : MonoBehaviour {
 	void Update ()
     {
         t += speed * Time.deltaTime;
-<<<<<<< HEAD
 	    if (t > 100) t = 0;
         transform.LookAt(path.GetPointAt(t));
 	    transform.position = path.GetPointAt(t);
-=======
-        transform.position = path.GetPositionByT(t);
->>>>>>> 84ad8a2f0574c8f4902090857981526688a4350f
     }
 }
