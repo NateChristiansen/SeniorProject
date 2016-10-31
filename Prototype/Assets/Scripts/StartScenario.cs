2292c1152f57b7029ead8ec88e0a4a9859b0b2fc
Nate Christiansen <natechristiansen42@gmail.com>
2016-10-31 15:20:13 -0400

    start StartScenario scenario script

---
+++ Prototype/Assets/Scripts/StartScenario.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScenario : MonoBehaviour {
+
+    GameObject vehicle;
+    bool playerAction = false;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) playerAction = true;
+    }
+}
