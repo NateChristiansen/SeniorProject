b3e1e18674bf3ce3a45b6db83f19e0f37698972f
jakewheeler <jakezro00@aim.com>
2016-11-08 15:43:22 -0500

    double tap works to recenter camera

---
+++ Prototype/Assets/Scripts/Recenter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Recenter : MonoBehaviour {
+
+	// Use this for initialization
+
+    ClickManager cm = new ClickManager();
+    public GvrViewer gvr;
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // PC/Mac
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (cm.DoubleClick())
+	        {
+                gvr.Recenter();
+	        }
+        }
+        // Mobile
+        else if (Input.touchCount > 0)
+        {
+            foreach (var touch in Input.touches)
+            {
+                if (touch.tapCount == 2)
+                {
+                    gvr.Recenter();
+                    Debug.Log("fffffff");
+                }
+            }
+        }
+
+	}
+}
