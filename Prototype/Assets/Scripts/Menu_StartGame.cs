4190c03c864ed9d3278abf719424bbab80c09f01
jakewheeler <jakezro00@aim.com>
2016-11-08 17:12:08 -0500

    created a start menu scene. It can load our test scene or quit out of the experience. works on mobile too

---
+++ Prototype/Assets/Scripts/Menu_StartGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu_StartGame : MonoBehaviour, IGvrGazeResponder {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnGazeEnter()
+    {
+        
+    }
+
+    public void OnGazeExit()
+    {
+        
+    }
+
+    public void OnGazeTrigger()
+    {
+        SceneManager.LoadScene("test");
+    }
+}
