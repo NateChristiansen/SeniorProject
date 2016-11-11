73e922a1d8944e8eab1988ccef7cf36c49166219
Nate Christiansen <natechristiansen42@gmail.com>
2016-11-11 13:11:51 -0500

    IK driver is imported

---
+++ Prototype/Assets/TurnTheGameOn/IK Driver/Scripts/DestroyAudio.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAudio : MonoBehaviour {
+
+	void Update () {
+		if(!GetComponent<AudioSource>().isPlaying){
+			Destroy(gameObject);
+		}
+	}
+}
