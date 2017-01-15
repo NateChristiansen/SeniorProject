074e8b555291c01af67cd6e884de01ec430ad007
jakewheeler <jakezro00@aim.com>
2017-01-14 19:05:31 -0500

    created a stop sign scenario. It is mostly feature complete but does not have an end result (it just outputs the user's choice to the debug console currently. This would work for a demo but the implementation needs to be revised to create multiple types of scenarios easily. Take a look and feel free to edit or throw me some ideas. Let the car drive for a few seconds and you'll understand

---
+++ Prototype/Assets/Scripts/DefaultSelectionHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultSelectionHandler : MonoBehaviour, IGvrGazeResponder {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnGazeEnter()
+    {
+        
+    }
+
+    public void OnGazeExit()
+    {
+        
+    }
+
+    public void OnGazeTrigger()
+    {
+        GameObject.Find("Vehicle").gameObject.GetComponent<StopSignScenario>().SetSelectionResult("default");
+        GameObject.Find("Vehicle").gameObject.GetComponent<StopSignScenario>().SetSelectionMade(true);
+        ColorChoice();
+    }
+
+    void ColorChoice()
+    {
+        foreach (Transform child in GameObject.Find("ScenarioMenu").transform)
+        {
+                child.gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+        this.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+}
---
+++ Prototype/Assets/Scripts/ScenarioListener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class ScenarioListener : MonoBehaviour {
+
+    public string GoodChoiceText;
+    public string BadChoiceText;
+    public string DefaultChoiceText;
+    public float Time;
+
+    // determine the scenario type
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "StopSignScenarioTrigger")
+        {
+            var stop = gameObject.AddComponent<StopSignScenario>();
+            stop.GoodChoiceText = GoodChoiceText;
+            stop.BadChoiceText = BadChoiceText;
+            stop.DefaultChoiceText = DefaultChoiceText;
+            stop.Timer = Time;
+        }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
