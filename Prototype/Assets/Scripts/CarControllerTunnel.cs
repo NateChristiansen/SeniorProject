eac6cae0ff5078380f72361ecfa4cad12cae1f77
jakewheeler <jakezro00@aim.com>
2017-01-27 12:51:39 -0500

    created tunnel for start scene with moving car

---
+++ Prototype/Assets/Scripts/CarControllerTunnel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControllerTunnel : MonoBehaviour {
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float step = Speed*Time.deltaTime;
+	    transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("endpos").transform.position, step);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        transform.position = GameObject.Find("startpos").transform.position;
+    }
+}
