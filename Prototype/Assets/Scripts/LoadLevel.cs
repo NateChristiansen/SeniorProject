d57758320027c758c1a044731607afbeada13c48
Nate Christiansen <natechristiansen42@gmail.com>
2017-02-08 17:43:08 -0500

    did things of some sort idr what

---
+++ Prototype/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+    private AsyncOperation async;
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(Load());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    IEnumerator Load()
+    {
+        async = SceneManager.LoadSceneAsync("spaceScene");
+        async.allowSceneActivation = false;
+        yield return async;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (async != null)
+            async.allowSceneActivation = true;
+    }
+}
