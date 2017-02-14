5ffe4930deef99c9806377044e744837f9fa4166
Nate Christiansen <natechristiansen42@gmail.com>
2017-02-14 18:14:21 -0500

    added some space assets. started trying to figure out spline branching

---
+++ Prototype/Assets/Scripts/CarSwerve.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Battlehub.SplineEditor;
+
+public class CarSwerve : MonoBehaviour
+{
+    public Spline OriginalPath, BranchingPath;
+    public MoveCar CarPath;
+    public RG_IKDriver Driver;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (Driver.LookTarget == RG_IKDriver.LookState.Straight)
+        {
+            CarPath.Path = BranchingPath;
+        }
+    }
+}
