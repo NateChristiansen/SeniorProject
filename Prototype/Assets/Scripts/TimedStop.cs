7c436c078c23aad1440bf94d053e6fd2af8b0c47
jakewheeler <jakezro00@aim.com>
2017-02-26 22:38:20 -0500

    in the process of making scenarios for lava scene

---
+++ Prototype/Assets/Scripts/TimedStop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedStop : MonoBehaviour {
+
+    public bool Continue, Stop;
+    public RG_IKDriver Driver;
+    public MoveCar DriverCar;
+    private float originalspeed;
+    public DriverControl Controller;
+    public float WaitTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        originalspeed = DriverCar.Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Stop && !Continue)
+            DriverCar.Speed = 0;
+        else
+            DriverCar.Speed = originalspeed;
+
+        if (Stop && Driver.LookTarget != RG_IKDriver.LookState.Straight)
+            Stop = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (Driver.LookTarget == RG_IKDriver.LookState.Straight)
+        {
+            Stop = true;
+            Controller.SolvedScenario = true;
+        }
+    }
+}
