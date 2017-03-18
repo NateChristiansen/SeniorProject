57c046f92848276310668f0849c4924522b1eb08
jakewheeler <jakezro00@aim.com>
2017-03-18 13:24:31 -0400

    added script to load in lava shader later instead of at start

---
+++ Prototype/Assets/Scripts/AssistingScripts/Lava Scene/Builder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Builder : MonoBehaviour
+{
+
+    public GameObject LavaGameObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("car"))
+        {
+            BuildLava();
+        }
+    }
+
+    private void BuildLava()
+    {
+        LavaGameObject.SetActive(true);
+    }
+}
