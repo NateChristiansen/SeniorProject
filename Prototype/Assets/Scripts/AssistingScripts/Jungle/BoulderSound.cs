a556fc64b673853f4841d74efda271a39f0f63ca
Nick <nick.kapty@yahoo.com>
2017-04-02 15:42:11 -0400

    added scenario 2

---
+++ Prototype/Assets/Scripts/AssistingScripts/Jungle/BoulderSound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderSound : MonoBehaviour {
+	
+	public AudioSource terrainSound;
+	public AudioSource carSound;
+	private int playTimes = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+
+		if (col.gameObject.tag.Equals("car"))
+		{
+			terrainSound.Play();
+			carSound.Play();
+			playTimes++;
+			// play rock and metal hitting sound
+		}
+	}
+
+}
