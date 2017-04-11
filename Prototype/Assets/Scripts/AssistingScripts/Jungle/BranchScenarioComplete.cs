1ebc9fd39c77357f828376d66bf9c48f88f50e5d
Nick <nick.kapty@yahoo.com>
2017-04-11 15:02:57 -0400

    now load/deleting point objects, animations are in but need polished, all completed scenarios are being counted

--- Prototype/Assets/Scripts/AssistingScripts/Jungle/BranchScenarioComplete.cs
+++ Prototype/Assets/Scripts/AssistingScripts/Jungle/BranchScenarioComplete.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GetScenario : MonoBehaviour {
+public class BranchScenarioComplete : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
